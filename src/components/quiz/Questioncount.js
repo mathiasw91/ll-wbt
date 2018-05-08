30e703a14bd9bd2f7c1379690687a94432cccca0
Mathias Widera <mathias.widera@akbs.de>
2018-05-08 13:59:53 +0200

    quiz progress bar eingebaut

--- src/components/quiz/Questioncount.js
+++ src/components/quiz/Questioncount.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import {Line} from 'rc-progress'
 
   function QuestionCount(props) {
+    let pct = (props.counter-1)/props.total*100
     return (
-      <div className="questionCount">
-        Question <span>{props.counter}</span> of <span>{props.total}</span>
-      </div>
+      <Line percent={pct} strokeWidth="1" strokeColor="green" />
     )
   }
 
