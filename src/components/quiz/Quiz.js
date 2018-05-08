7222e60dc927d2140415068130801b13c8f4a09b
Mathias Widera <mathias.widera@akbs.de>
2018-05-08 15:03:15 +0200

    Quizlogik umgeschrieben

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -11,11 +11,23 @@ class Quiz extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     currentQuestion: 0,
+     currentQuestion: 0,  //Index für diese Session
+     questions: props.questions,  //Fragen für diese Session
      answer: false,
      answerWrong: false,
-     questionsAnswered: [],
-     questionTotal: props.questions.length
+     questionsAnswered: props.total - props.questions.length, //Anz. aller beantworteten Fragen(sessionübergreifend)
+     questionsTotal: props.total, //Anz. aller Fragen(sessionübergreifend)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.total !== this.state.questionsTotal) {
+      this.setState({ questionsTotal: nextProps.total });
+    }
+    this.setState({questionsAnswered: nextProps.total - nextProps.questions.length})
+    if(this.updateQuestions) {
+      this.setState({questions: nextProps.questions})
+      this.updateQuestions = false
     }
   }
 
@@ -23,44 +35,37 @@ class Quiz extends React.Component {
   render() {
     return (
       <div id="quiz">
-        {this.state.currentQuestion > (this.state.questionTotal-1) && (
-          <Score total={this.state.questionTotal} correct={this.state.questionsAnswered.length}/>
+        {this.state.currentQuestion >= this.state.questions.length && (
+          <Score total={this.state.questionsTotal} correct={this.state.questionsAnswered}
+          alreadyFinished={(this.state.questionsAnswered == this.state.questionsTotal) && this.state.currentQuestion == 0}/>
         )}
-        {this.state.currentQuestion < this.props.questions.length && (
+        {this.state.currentQuestion < this.state.questions.length && (
           <div>
             <QuestionCount
-               counter={this.state.currentQuestion+1}
-               total={this.props.questions.length}
+               counter={this.state.questionsAnswered}
+               total={this.state.questionsTotal}
              />
-            <Quizpage question={this.props.questions[this.state.currentQuestion]} answer={this.state.answer} answerWrong={this.state.answerWrong} setUserAnswer={this.setUserAnswer.bind(this)} navigateNext={this.nextQuestion.bind(this)}/>
-            <div style={{marginTop:"1em"}}>
-              <Link className="btn btn-default" to={this.props.location.pathname.replace('/quiz','')}>Zurück zum Lerninhalt</Link>
-              <button className="btn btn-default" style={{marginLeft:"2em"}} onClick={this.resetQuiz.bind(this)}>Dieses Quiz zurücksetzen</button>
-            </div>
+            <Quizpage question={this.state.questions[this.state.currentQuestion]} answer={this.state.answer} answerWrong={this.state.answerWrong} setUserAnswer={this.setUserAnswer.bind(this)} navigateNext={this.nextQuestion.bind(this)}/>
           </div>
         )}
+        <div style={{marginTop:"1em"}}>
+          <Link className="btn btn-default" to={this.props.location.pathname.replace('/quiz','')}>Zurück zum Lerninhalt</Link>
+          <button className="btn btn-default" style={{marginLeft:"2em"}} onClick={this.resetQuiz.bind(this)}>Dieses Quiz zurücksetzen</button>
+        </div>
       </div>
     )
   }
 
   nextQuestion(){
-    this.setState({currentQuestion: this.state.currentQuestion+1, answer: false, answerWrong: false}, ()=>{
-      if(this.state.currentQuestion > this.props.questions.length-1){
-        this.state.questionsAnswered.forEach(q=>{
-          AppActions.setQuestionAnswered(q)
-        })
-      }
-    })
+    this.setState({currentQuestion: this.state.currentQuestion+1, answer: false, answerWrong: false})
   }
 
   setUserAnswer(answer){
     this.setState({
       answer: parseInt(answer)
     });
-    if(this.props.questions[this.state.currentQuestion].answers[answer].correct){
-        let qa = this.state.questionsAnswered
-        qa.push(this.props.questions[this.state.currentQuestion].id)
-        this.setState({questionsAnswered: qa})
+    if(this.state.questions[this.state.currentQuestion].answers[answer].correct){
+        AppActions.setQuestionAnswered(this.state.questions[this.state.currentQuestion].id)
     }else{
       this.setState({answerWrong: true})
     }
@@ -68,7 +73,8 @@ class Quiz extends React.Component {
 
   resetQuiz(){
     AppActions.resetQuiz();
-    this.setState({currentQuestion:0, answerWrong: false, answer: false, questionsAnswered: []})
+    this.updateQuestions = true
+    this.setState({currentQuestion:0, answerWrong: false, answer: false})
   }
 
 }
