1ff6b9a72d12802a95910f539b220f76d5aa1613
Mathias Widera <mathias.widera@akbs.de>
2018-05-08 15:42:05 +0200

    Quiz Auswertung verbessert

--- src/components/quiz/Score.js
+++ src/components/quiz/Score.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {withRouter, Link} from 'react-router-dom'
+import {Circle} from 'rc-progress'
 
   function Score(props) {
     if(props.alreadyFinished) return (
@@ -9,13 +10,23 @@ import {withRouter, Link} from 'react-router-dom'
         <Link className="btn btn-default" to={props.location.pathname.replace('/quiz','')}>Zum nächsten Thema</Link>
       </div>
     )
+
+    let pctg = props.correct/props.total*100
+    let circleColor = getColor(pctg/100)
+
     return (
-      <div id="quiz-score">
-        {props.correct} / {props.total} Fragen richtig beantwortet!
-        <Link className="btn btn-default" to={props.location.pathname.replace('/quiz','')}>Zurück zum Lerninhalt</Link>
+      <div id="quiz-score" className="result">
+        <span>{props.correct} / {props.total} Fragen richtig beantwortet!</span>
+        <Circle percent={pctg} strokeWidth="4" strokeColor={circleColor} gapDegree="360"/>
       </div>
     )
   }
+
+  function getColor(value){
+    //value from 0 to 1
+    var hue=(value*120).toString(10);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
 
 
   export default withRouter(Score)
