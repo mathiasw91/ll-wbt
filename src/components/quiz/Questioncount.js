a56f17b049f73bf999d336cf9ddf35835b93df74
Mathias Widera <mathias.widera@akbs.de>
2018-05-09 12:54:33 +0200

    Quiz zusätzlich Textprogress eingebaut

--- src/components/quiz/Questioncount.js
+++ src/components/quiz/Questioncount.js
@@ -3,9 +3,10 @@ import {Line} from 'rc-progress'
 
   function QuestionCount(props) {
     let pct = props.counter/props.total*100
-    return (
+    return (<div>
       <Line percent={pct} strokeWidth="1" strokeColor="green" />
-    )
+      <div className="text-progress">{props.counter} von {props.total} Fragen gelöst</div>
+    </div>)
   }
 
   export default QuestionCount
