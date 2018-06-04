2099007004c47f8db6d34eedec98d90a8a038ae9
Mathias Widera <mathias.widera@akbs.de>
2018-06-04 20:36:55 +0200

    Textbeschreibung säen

--- src/pages/aussaat-und-bepflanzung.js
+++ src/pages/aussaat-und-bepflanzung.js
@@ -3,7 +3,7 @@ import React from "react"
 
 class AussaatUndBepflanzung extends React.Component {
 
-
+//https://www.magicgardenseeds.de/Aussaatanleitungen/Allgemeine-Aussaatanleitung-f%C3%BCr-Blumen-Kr%C3%A4uter-Gem%C3%BCse-T.134-/
 
   render() {
     return (
@@ -12,9 +12,10 @@ class AussaatUndBepflanzung extends React.Component {
 
         <h2><i class="fa fa-leaf"></i> Säen</h2>
         <p>Sie können das Gemüsebeet nun mit gekauften oder vorgezogenen Jungpflanzen bepflanzen oder Saatgut einsäen. Wenn der Boden sehr trocken ist, sollten Sie die Rillen vor der Aussaat gießen, sodass die Samen genug Feuchtigkeit abbekommen. Die Aussaattermine der verschiedenen Gemüsearten stehen auf den Saattüten. Warten Sie mit der Aussaat grundsätzlich bis etwa April, dann hat sich der Boden des Gemüsebeetes genügend erwärmt. Sehr sandhaltige Böden erwärmen sich schneller als schwere, tonhaltige Erden. Hier können Sie unter Umständen schon etwas früher loslegen. Kündigen sich kühle und regenreiche Tage an, verschieben Sie die Aussaat um einige Tage oder Wochen.</p>
+        <p> Bereiten Sie das Beet gut vor: Der Boden sollte gelockert und fein krümelig sowie möglichst unkrautfrei sein. Es kann entweder in Reihen oder auch breitwürfig ausgesät werden. In der Reihe lässt sich ganz einfach eine Rinne mit der gewünschten Aussaattiefe ziehen, in der dann die Samen mit dem entsprechenden Abstand platziert werden können. Anschließend die Samen mit Erde bedecken, gut andrücken und mit weichem Wasserstrahl angießen.</p>
 
         <h2><i class="fa fa-leaf"></i> Hinweise zum Säen</h2>
-        <p>Beachten Sie den für die einzelnen Arten empfohlenen Abstand. Feuchte Säcke über dem Saatbeet garantieren, dass das Saatgut nicht so schnell austrocknet. Feines Saatgut wie das von Möhren gleichmäßig auszusäen, ist nicht ganz einfach. Bei der Reihensaat gelangen an einigen Stellen zu viele Samen in die Erde, an anderen zu wenige. Damit die Pflanzen sich nicht gegenseitig bedrängen, muss man die jungen Sämlinge frühzeitig "verziehen", also die zu dicht gesäten Stellen ausdünnen, indem man die schwächsten Pflanzen entfernt. Alternativ können Sie die Samen mit trockenem Quarzsand mischen und zusammen in die Saatrillen streuen. </p>
+        <p>Beachten Sie den für die einzelnen Arten empfohlenen Abstand. Feuchtes Vlies über der Aussaatstelle garantieren, dass das Saatgut nicht so schnell austrocknet. Feines Saatgut wie das von Möhren gleichmäßig auszusäen, ist nicht ganz einfach. Bei der Reihensaat gelangen an einigen Stellen zu viele Samen in die Erde, an anderen zu wenige. Damit die Pflanzen sich nicht gegenseitig bedrängen, muss man die jungen Sämlinge frühzeitig "verziehen", also die zu dicht gesäten Stellen ausdünnen, indem man die schwächsten Pflanzen entfernt. Alternativ können Sie die Samen mit trockenem Quarzsand mischen und zusammen in die Saatrillen streuen. </p>
 
 
         <h2><i class="fa fa-leaf"></i> Bepflanzen</h2>
